dfa8c822e77a574852e9bc19126bc2510071910d
Hayden Bryant <haydenbryant.student@careerdevs.com>
2021-01-11 19:51:38 -0500

    cyclicRotation. completed 87%. just need to handle possible empty array

--- src/com/spring/hockeystats/Main.java
+++ src/com/spring/hockeystats/Main.java
@@ -5,27 +5,19 @@
 // System.out.println("this is a debug message");
 
 class Solution {
-    public int solution(int[] A) {
+    public int[] solution(int[] A, int K) {
         // write your code in Java SE 8
-        int leftTotal = A[0];
-        int rightTotal = 0;
-
-        for(int i = 0; i < A.length; i++){
-            rightTotal += A[i];
-        }
-        rightTotal -= leftTotal;
-
-        int dif = Math.abs(leftTotal - rightTotal);
-        int minDif = dif;
-
-        for(int i = 1; i < A.length; i++){
-            leftTotal += A[i];
-            rightTotal -= A[i];
-            dif = Math.abs(leftTotal - rightTotal);
-            if(dif < minDif){
-                minDif = dif;
+        for(int i = 0; i < K; i++){
+            int finalElm = A[A.length - 1];
+            int lastElm = A[0];
+            for(int j = 1; j < A.length; j++){
+                int currElm = A[j];
+                A[j] = lastElm;
+                lastElm = currElm;
             }
+            A[0] = finalElm;
         }
-        return minDif;
+
+        return A;
     }
 }
