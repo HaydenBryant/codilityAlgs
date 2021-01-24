11733b92d758d2241b0e71cd6d760dfc4a17d707
Hayden Bryant <haydenbryant.student@careerdevs.com>
2021-01-24 14:28:37 -0500

    Dominator. finished 100% but can easily be refactored

--- src/com/spring/hockeystats/Main.java
+++ src/com/spring/hockeystats/Main.java
@@ -1,36 +1,38 @@
 // you can also use imports, for example:
 // import java.util.*;
-import java.util.Stack;
+import java.util.HashMap;
 
 // you can write to stdout for debugging purposes, e.g.
 // System.out.println("this is a debug message");
 
 class Solution {
-    public int solution(String S) {
+    public int solution(int[] A) {
         // write your code in Java SE 8
-        Stack<Character> stack = new Stack<Character>();
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
 
-        for(char c: S.toCharArray()){
-            if(c == '{' || c == '[' || c == '('){
-                stack.push(c);
-            } else if (c == '}'){
-                if(stack.isEmpty() || stack.pop() != '{'){
-                    return 0;
-                }
-            } else if (c == ']'){
-                if(stack.isEmpty() || stack.pop() != '['){
-                    return 0;
-                }
-            } else if (c == ')'){
-                if(stack.isEmpty() || stack.pop() != '('){
-                    return 0;
-                }
+        for(int num : A){
+            if(!map.containsKey(num)){
+                map.put(num, 1);
+            } else {
+                map.put(num, map.get(num) + 1);
             }
         }
-        if(stack.isEmpty()){
-            return 1;
-        } else {
-            return 0;
+
+        int dominator = -1;
+
+        for(int num : map.keySet()){
+            if(map.get(num) > A.length / 2){
+                dominator = num;
+            }
         }
+
+        for(int i = 0; i < A.length; i++){
+            if(A[i] == dominator){
+                dominator = i;
+                break;
+            }
+        }
+
+        return dominator;
     }
 }
